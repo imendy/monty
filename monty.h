e4f72d3c400172a7c7cb7ca9fac52a153e6db029
EMMANUEL NDUKWE NMAJU <enmaju37@gmail.com>
2023-06-22 17:51:19 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -1,13 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 
 /**
  * struct globals - variables globales
